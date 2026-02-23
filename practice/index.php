cd2151e7dd8371316c9c311ac3375bb9a281977d
MUi2Z <mmuizzuddin.teen@gmail.com>
2026-02-23 08:29:17 +0800

    temporary sound replacement

--- practice/index.php
+++ practice/index.php
@@ -132,8 +132,10 @@ $result = mysqli_query($conn, $query);
         <div class="absolute left-1/2 h-full w-[2px] bg-black/5 -translate-x-1/2"></div>
     </div>
 
-    <audio id="sound-pak" src="../src/sfx/pak.mp3"></audio>
-    <audio id="sound-tung" src="../src/sfx/tung.mp3"></audio>
+    <audio id="sound-pak" src="../src/sfx/snare.mp3"></audio>
+    <audio id="sound-tung" src="../src/sfx/bass-drum.mp3"></audio>
+    <!-- <audio id="sound-pak" src="../src/sfx/pak.mp3"></audio>
+    <audio id="sound-tung" src="../src/sfx/tung.mp3"></audio> -->
 
     <script src="../src/js/game.js"></script>
 </div>
